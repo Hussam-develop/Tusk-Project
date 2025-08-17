71e45c39b6f523becfc92bdefddcafa97360e5fb
Hussam-develop <hussamalsyedkhalil@gmail.com>
2025-08-17 18:06:54 +0300

    Updatetest message

--- routes/api.php
+++ routes/api.php
@@ -23,7 +23,7 @@ use App\Http\Controllers\LabManager\ClientsController;
 use App\Http\Controllers\LabManager\EmployeesController;
 
 Route::get('test', function () {
-    return "******** 💚 Welcome to Tusk platform 💚  **********";
+    return "     Welcome to Tusk platform   ";
 });
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
